4b552b7ba0d7a215deed2ca55243562936eebfe0
Deni Gunawan <artdengun@gmail.com>
2022-03-24 20:50:21 +0700

    spring-publisher-app

---
+++ src/main/java/com/deni/gunawan/springpublisherapp/SpringPublisherAppApplication.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.springpublisherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPublisherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPublisherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/gunawan/springpublisherapp/SpringPublisherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.springpublisherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPublisherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
